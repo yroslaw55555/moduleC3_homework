aa8b5570ea1b7c560ec36707bbf0bc2a18ba47dc
yroslaw55555 <75557043+yroslaw55555@users.noreply.github.com>
2021-07-01 16:50:39 +0700

    Add files via upload

---
+++ C3.5.js
@@ -0,0 +1,79 @@
+/*jshint esversion: 6 */
+class ElectroOnOff {
+    constructor(name, power) {
+		this.name = name;
+		this.power = power;
+		this.ISKEY = false;
+	}
+
+	KeyOn() {
+		console.log(this.name + " vkluchilsia");
+		this.ISKEY = true;
+	}
+
+	KeyOff() {
+		console.log(this.name + " viiikluchilsia");
+		this.ISKEY = false;
+	}
+}
+
+class GenerationNoise extends ElectroOnOff {
+	constructor(name, power) {
+		super(name, power);
+        this.isKey = this.ISKEY;
+        this.secretProperty = "Пылесосит";
+	}
+
+    showProp() {
+        if (this.isKey) {
+			this.noise = true;
+			this.secretProperty = "Пылесосит";
+		} else {
+			this.noise = false;
+			this.secretProperty = "Не пылесосит";
+		}
+        return this.secretProperty;
+    }
+}
+
+class Teapot extends ElectroOnOff {
+	constructor(name, power) {
+		super(name, power);
+        this.isKey = this.ISKEY;
+        this.warmUp = true;
+	}
+
+    showProp() {
+        if (this.isKey) {
+			this.warmUp = true;
+		} else {
+			this.warmUp = false;
+		}
+        return this.warmUp;
+    }
+}
+
+
+const BadDevice = new GenerationNoise("Пылесос Toshoiba", 50);
+const TeapotOne = new Teapot("Vitek", 50);
+
+BadDevice.KeyOn();
+TeapotOne.KeyOff();
+
+
+console.log(GenerationNoise.name + "is " + GenerationNoise.showProp());
+console.log(Teapot.name + "is " + Teapot.showProp());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
